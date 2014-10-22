9602c3f24686b1ad12861945e15225e87f3fd295
Hamid Nazari <hn@linux.com>
2014-10-22 22:15:09 +1100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(name='awsclpy',
+      version='0.1',
+      description='Chain AWSCLI commands.',
+      long_description='Run AWSCLI commands and use their outputs in next commands.',
+      classifiers=[
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Operating System :: OS Independent',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Development Status :: 4 - Beta',
+      ],
+      keywords=[
+          'awsclpy',
+          'awscli',
+          'amazon web services',
+          'command line interface',
+      ],
+      author='Hamid Nazari',
+      author_email='hn@linux.com',
+      url='http://github.com/hamidnazari/awsclpy',
+      license='MIT',
+      packages=['awsclpy'],
+      install_requires=[
+          "awscli",
+      ],
+      zip_safe=False)
